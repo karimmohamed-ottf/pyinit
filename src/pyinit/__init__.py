f14a0cb5838ff8b01caa1ce0b7e06598d617aa11
Karim <kwkkna3@gnail.com>
2025-11-07 11:41:35 +0200

    adding __init__.py files to project/templates

---
+++ src/pyinit/__init__.py
@@ -0,0 +1,26 @@
+"""
+pyinit - Your All-in-One Python Project Manager
+
+"""
+
+__version__ = "1.0.0" 
+from .new_project import create_project
+from .init_project import initialize_project
+from .run_project import start_project
+from .add_module import install_module
+from .lock_project import lock_dependencies
+from .update_project import update_dependencies
+from .build_project import install_project
+from .test_project import run_tests
+from .lint_project import lint_project
+from .format_project import format_project
+from .graph_project import show_dependency_graph
+from .clean_project import clean_project
+from .scan_project import scan_project
+from .bump_project import bump_project_version
+from .venv_manager import manage_venv
+from .dockerize_project import dockerize_project
+from .env_manager import manage_env
+from .license_manager import manage_license
+from .hooks_manager import add_git_hooks
+from .main import main
